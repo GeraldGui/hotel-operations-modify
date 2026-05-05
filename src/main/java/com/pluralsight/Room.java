60e2f3897b8cc79cb3501171d811f50fb08013cb
Gerald G <geraldguignard2@gmail.com>
2026-05-05 09:16:29 -0400

    Made 3 methods in the Room class for checkIn, checkout, and cleanroom.

--- src/main/java/com/pluralsight/Room.java
+++ src/main/java/com/pluralsight/Room.java
@@ -1,4 +1,51 @@
 package com.pluralsight;
 
 public class Room {
+    private int numberOfBeds;
+    private double price;
+    private boolean isOccupied;
+    private boolean isDirty;
+
+    public Room(int numberOfBeds, double price) {
+        this.numberOfBeds = numberOfBeds;
+        this.price = price;
+        this.isOccupied = false;
+        this.isDirty = false;
+    }
+
+    public int getNumberOfBeds() {
+        return numberOfBeds;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isOccupied() {
+        return isOccupied;
+    }
+
+    public boolean isDirty() {
+        return isDirty;
+    }
+
+    public boolean isAvailable() {
+        return !isOccupied && !isDirty;
+    }
+
+    public void checkIn() {
+        isOccupied = true;
+        isDirty = true;
+        System.out.println("Room is not available.");
+    }
+
+    public void checkout() {
+        cleanroom();
+        System.out.println("Room is getting clean then will be available.");
+        isAvailable();
+    }
+
+    public void cleanroom() {
+        isDirty = false;
+    }
 }
